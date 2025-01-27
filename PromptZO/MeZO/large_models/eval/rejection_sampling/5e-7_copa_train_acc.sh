a6d8d8269c5a6b7df86aad4e5769b5230084d4b6
Rongite <rongite2022@gmail.com>
2025-01-27 15:43:41 +0000

    run first temp=0.9

--- PromptZO/MeZO/large_models/eval/rejection_sampling/5e-7_copa_train_acc.sh
+++ PromptZO/MeZO/large_models/eval/rejection_sampling/5e-7_copa_train_acc.sh
@@ -15,7 +15,7 @@
 # polaris
 
 python /grand/sbi-fair/jikaiLoong/Synthetic_Data_for_ZO/PromptZO/MeZO/large_models/eval_acc.py \
-    --model_name /grand/sbi-fair/jikaiLoong/models/synthetic_data/zo/synthetic/Copa-Llama-3.2-1B-mezo-meta-llama/Llama-3.2-1B-/grand/sbi-fair/jikaiLoong/Synthetic_Data_for_ZO/Data/rejection_sampling/mezo/Copa-ft-16-5e-7-1e-3-0/checkpoint-8500 \
+    --model_name /grand/sbi-fair/jikaiLoong/models/synthetic_data/zo/synthetic/Copa-Llama-3.2-1B-mezo-meta-llama/Llama-3.2-1B-/grand/sbi-fair/jikaiLoong/Synthetic_Data_for_ZO/Data/rejection_sampling/mezo/Copa-ft-16-5e-7-1e-3-0/checkpoint-8000 \
     --task_name /grand/sbi-fair/jikaiLoong/Synthetic_Data_for_ZO/Data/rejection_sampling/mezo/Copa \
     --output_dir ./result \
     --num_train 1000 \
