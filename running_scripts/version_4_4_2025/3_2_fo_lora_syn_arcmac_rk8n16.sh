89bbe44c436a39bf2d5faa201a1972c5cc341ae8
Jikai Long <rongite2022@gmail.com>
2025-04-15 17:15:10 -0400

    Ignore .out and .err files from now on

---
+++ running_scripts/version_4_4_2025/3_2_fo_lora_syn_arcmac_rk8n16.sh
@@ -0,0 +1,47 @@
+#!/bin/bash -l
+
+cd /grand/sbi-fair/jikaiLoong/Synthetic_Data_for_ZO/PromptZO/MeZO/large_models
+
+OUT_0=/grand/sbi-fair/jikaiLoong/Synthetic_Data_for_ZO/Data/rejection_sampling/3_first_order_lora/result/ArcC_MC/llama_3.2_1b/1e-4_lora_rk8.out
+ERR_0=/grand/sbi-fair/jikaiLoong/Synthetic_Data_for_ZO/Data/rejection_sampling/3_first_order_lora/result/ArcC_MC/llama_3.2_1b/1e-4_lora_rk8.err
+CUDA_VISIBLE_DEVICES=0,1  MODEL=meta-llama/Llama-3.2-1B MODE=ft TASK=/grand/sbi-fair/jikaiLoong/Synthetic_Data_for_ZO/Data/rejection_sampling/0_data/ArcC_MC LR=1e-4 BS=16 RANK=8 STEPS=20000 SEED=0 bash fo_lora_finetune_synthetic.sh 1>>$OUT_0 2>>$ERR_0 &
+
+OUT_1=/grand/sbi-fair/jikaiLoong/Synthetic_Data_for_ZO/Data/rejection_sampling/3_first_order_lora/result/ArcC_MC/llama_3.2_1b/2e-4_lora_rk8.out
+ERR_1=/grand/sbi-fair/jikaiLoong/Synthetic_Data_for_ZO/Data/rejection_sampling/3_first_order_lora/result/ArcC_MC/llama_3.2_1b/2e-4_lora_rk8.err
+CUDA_VISIBLE_DEVICES=2,3  MODEL=meta-llama/Llama-3.2-1B MODE=ft TASK=/grand/sbi-fair/jikaiLoong/Synthetic_Data_for_ZO/Data/rejection_sampling/0_data/ArcC_MC LR=2e-4 BS=16 RANK=8 STEPS=20000 SEED=0 bash fo_lora_finetune_synthetic.sh 1>>$OUT_1 2>>$ERR_1 &
+
+wait
+
+OUT_2=/grand/sbi-fair/jikaiLoong/Synthetic_Data_for_ZO/Data/rejection_sampling/3_first_order_lora/result/ArcC_MC/llama_3.2_1b/1e-4_lora_rk16.out
+ERR_2=/grand/sbi-fair/jikaiLoong/Synthetic_Data_for_ZO/Data/rejection_sampling/3_first_order_lora/result/ArcC_MC/llama_3.2_1b/1e-4_lora_rk16.err
+CUDA_VISIBLE_DEVICES=0,1  MODEL=meta-llama/Llama-3.2-1B MODE=ft TASK=/grand/sbi-fair/jikaiLoong/Synthetic_Data_for_ZO/Data/rejection_sampling/0_data/ArcC_MC LR=1e-4 BS=16 RANK=16 STEPS=20000 SEED=0 bash fo_lora_finetune_synthetic.sh 1>>$OUT_2 2>>$ERR_2 &
+
+
+OUT_3=/grand/sbi-fair/jikaiLoong/Synthetic_Data_for_ZO/Data/rejection_sampling/3_first_order_lora/result/ArcC_MC/llama_3.2_1b/2e-4_lora_rk16.out
+ERR_3=/grand/sbi-fair/jikaiLoong/Synthetic_Data_for_ZO/Data/rejection_sampling/3_first_order_lora/result/ArcC_MC/llama_3.2_1b/2e-4_lora_rk16.err
+CUDA_VISIBLE_DEVICES=2,3  MODEL=meta-llama/Llama-3.2-1B MODE=ft TASK=/grand/sbi-fair/jikaiLoong/Synthetic_Data_for_ZO/Data/rejection_sampling/0_data/ArcC_MC LR=2e-4 BS=16 RANK=16 STEPS=20000 SEED=0 bash fo_lora_finetune_synthetic.sh 1>>$OUT_3 2>>$ERR_3 &
+
+wait
+
+# cd /home/jlong1/Downloads/Synthetic_Data_for_ZO/PromptZO/MeZO/large_models
+
+# OUT_0=/home/jlong1/Downloads/Synthetic_Data_for_ZO/Data/rejection_sampling/3_first_order_lora/result/ArcC_MC/llama_3.2_1b/1e-4_lora_rk8.out
+# ERR_0=/home/jlong1/Downloads/Synthetic_Data_for_ZO/Data/rejection_sampling/3_first_order_lora/result/ArcC_MC/llama_3.2_1b/1e-4_lora_rk8.err
+# CUDA_VISIBLE_DEVICES=0 MODEL=meta-llama/Llama-3.2-1B MODE=ft TASK=/home/jlong1/Downloads/Synthetic_Data_for_ZO/Data/rejection_sampling/0_data/ArcC_MC LR=1e-4 BS=16 RANK=8 STEPS=20000 SEED=0 bash fo_lora_finetune_synthetic.sh 1>>$OUT_0 2>>$ERR_0 &
+
+# OUT_2=/home/jlong1/Downloads/Synthetic_Data_for_ZO/Data/rejection_sampling/3_first_order_lora/result/ArcC_MC/llama_3.2_1b/1e-4_lora_rk16.out
+# ERR_2=/home/jlong1/Downloads/Synthetic_Data_for_ZO/Data/rejection_sampling/3_first_order_lora/result/ArcC_MC/llama_3.2_1b/1e-4_lora_rk16.err
+# CUDA_VISIBLE_DEVICES=1 MODEL=meta-llama/Llama-3.2-1B MODE=ft TASK=/home/jlong1/Downloads/Synthetic_Data_for_ZO/Data/rejection_sampling/0_data/ArcC_MC LR=1e-4 BS=16 RANK=16 STEPS=20000 SEED=0 bash fo_lora_finetune_synthetic.sh 1>>$OUT_2 2>>$ERR_2 &
+
+# wait
+
+# OUT_1=/home/jlong1/Downloads/Synthetic_Data_for_ZO/Data/rejection_sampling/3_first_order_lora/result/ArcC_MC/llama_3.2_1b/2e-4_lora_rk8.out
+# ERR_1=/home/jlong1/Downloads/Synthetic_Data_for_ZO/Data/rejection_sampling/3_first_order_lora/result/ArcC_MC/llama_3.2_1b/2e-4_lora_rk8.err
+# CUDA_VISIBLE_DEVICES=0 MODEL=meta-llama/Llama-3.2-1B MODE=ft TASK=/home/jlong1/Downloads/Synthetic_Data_for_ZO/Data/rejection_sampling/0_data/ArcC_MC LR=2e-4 BS=16 RANK=8 STEPS=20000 SEED=0 bash fo_lora_finetune_synthetic.sh 1>>$OUT_1 2>>$ERR_1 &
+
+# OUT_3=/home/jlong1/Downloads/Synthetic_Data_for_ZO/Data/rejection_sampling/3_first_order_lora/result/ArcC_MC/llama_3.2_1b/2e-4_lora_rk16.out
+# ERR_3=/home/jlong1/Downloads/Synthetic_Data_for_ZO/Data/rejection_sampling/3_first_order_lora/result/ArcC_MC/llama_3.2_1b/2e-4_lora_rk16.err
+# CUDA_VISIBLE_DEVICES=1 MODEL=meta-llama/Llama-3.2-1B MODE=ft TASK=/home/jlong1/Downloads/Synthetic_Data_for_ZO/Data/rejection_sampling/0_data/ArcC_MC LR=2e-4 BS=16 RANK=16 STEPS=20000 SEED=0 bash fo_lora_finetune_synthetic.sh 1>>$OUT_3 2>>$ERR_3 &
+
+# wait
+
---
+++ running_scripts/version_4_4_2025/3_arcmc.sh
@@ -0,0 +1,7 @@
+#!/bin/bash -l
+
+bash 3_1_fo_full_orig_arcmc.sh
+bash 3_1_fo_full_syn_arcmc.sh
+bash 3_2_fo_lora_orig_arcmc_rk8n16.sh
+bash 3_2_fo_lora_orig_n_syn_arcmc_rk32.sh
+bash 3_2_fo_lora_syn_arcmac_rk8n16.sh
