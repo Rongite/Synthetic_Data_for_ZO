db1488151446ce15cbb598e8e6e3fc9219bacc2e
Rongite <rongite2022@gmail.com>
2025-04-17 04:23:06 +0000

    run original

--- running_scripts/version_4_4_2025/1_1_fo_full_orig_copa.sh
+++ running_scripts/version_4_4_2025/1_1_fo_full_orig_copa.sh
@@ -2,20 +2,20 @@
 
 cd /home/jlong1/Downloads/Synthetic_Data_for_ZO/PromptZO/MeZO/large_models
 
-OUT_0=/home/jlong1/Downloads/Synthetic_Data_for_ZO/Data/original/2_first_order_full/result/Copa/llama_3.2_1b/1e-6_synthetic.out
-ERR_0=/home/jlong1/Downloads/Synthetic_Data_for_ZO/Data/original/2_first_order_full/result/Copa/llama_3.2_1b/1e-6_synthetic.err
+OUT_0=/home/jlong1/Downloads/Synthetic_Data_for_ZO/Data/original/2_first_order_full/result/Copa/llama_3.2_1b/1e-6_original.out
+ERR_0=/home/jlong1/Downloads/Synthetic_Data_for_ZO/Data/original/2_first_order_full/result/Copa/llama_3.2_1b/1e-6_original.err
 CUDA_VISIBLE_DEVICES=0 MODEL=meta-llama/Llama-3.2-1B MODE=ft TASK=/home/jlong1/Downloads/Synthetic_Data_for_ZO/Data/original/Copa LR=1e-6 BS=16 STEPS=20000 SEED=0 bash fo_full_finetune_original.sh 1>>$OUT_0 2>>$ERR_0 & 
 
-OUT_1=/home/jlong1/Downloads/Synthetic_Data_for_ZO/Data/original/2_first_order_full/result/Copa/llama_3.2_1b/5e-7_synthetic.out
-ERR_1=/home/jlong1/Downloads/Synthetic_Data_for_ZO/Data/original/2_first_order_full/result/Copa/llama_3.2_1b/5e-7_synthetic.err
+OUT_1=/home/jlong1/Downloads/Synthetic_Data_for_ZO/Data/original/2_first_order_full/result/Copa/llama_3.2_1b/5e-7_original.out
+ERR_1=/home/jlong1/Downloads/Synthetic_Data_for_ZO/Data/original/2_first_order_full/result/Copa/llama_3.2_1b/5e-7_original.err
 CUDA_VISIBLE_DEVICES=0 MODEL=meta-llama/Llama-3.2-1B MODE=ft TASK=/home/jlong1/Downloads/Synthetic_Data_for_ZO/Data/original/Copa LR=5e-7 BS=16 STEPS=20000 SEED=0 bash fo_full_finetune_original.sh 1>>$OUT_1 2>>$ERR_1 &
 
-OUT_2=/home/jlong1/Downloads/Synthetic_Data_for_ZO/Data/original/2_first_order_full/result/Copa/llama_3.2_1b/2e-7_synthetic.out
-ERR_2=/home/jlong1/Downloads/Synthetic_Data_for_ZO/Data/original/2_first_order_full/result/Copa/llama_3.2_1b/2e-7_synthetic.err
+OUT_2=/home/jlong1/Downloads/Synthetic_Data_for_ZO/Data/original/2_first_order_full/result/Copa/llama_3.2_1b/2e-7_original.out
+ERR_2=/home/jlong1/Downloads/Synthetic_Data_for_ZO/Data/original/2_first_order_full/result/Copa/llama_3.2_1b/2e-7_original.err
 CUDA_VISIBLE_DEVICES=1 MODEL=meta-llama/Llama-3.2-1B MODE=ft TASK=/home/jlong1/Downloads/Synthetic_Data_for_ZO/Data/original/Copa LR=2e-7 BS=16 STEPS=20000 SEED=0 bash fo_full_finetune_original.sh 1>>$OUT_2 2>>$ERR_2 &
 
-OUT_3=/home/jlong1/Downloads/Synthetic_Data_for_ZO/Data/original/2_first_order_full/result/Copa/llama_3.2_1b/1e-7_synthetic.out
-ERR_3=/home/jlong1/Downloads/Synthetic_Data_for_ZO/Data/original/2_first_order_full/result/Copa/llama_3.2_1b/1e-7_synthetic.err
+OUT_3=/home/jlong1/Downloads/Synthetic_Data_for_ZO/Data/original/2_first_order_full/result/Copa/llama_3.2_1b/1e-7_original.out
+ERR_3=/home/jlong1/Downloads/Synthetic_Data_for_ZO/Data/original/2_first_order_full/result/Copa/llama_3.2_1b/1e-7_original.err
 CUDA_VISIBLE_DEVICES=1 MODEL=meta-llama/Llama-3.2-1B MODE=ft TASK=/home/jlong1/Downloads/Synthetic_Data_for_ZO/Data/original/Copa LR=1e-7 BS=16 STEPS=20000 SEED=0 bash fo_full_finetune_original.sh 1>>$OUT_3 2>>$ERR_3 &
 
 wait
--- running_scripts/version_4_4_2025/3_1_fo_full_orig_arcmc.sh
+++ running_scripts/version_4_4_2025/3_1_fo_full_orig_arcmc.sh
@@ -4,22 +4,22 @@
 
 cd /grand/sbi-fair/jikaiLoong/Synthetic_Data_for_ZO/PromptZO/MeZO/large_models
 
-OUT_0=/grand/sbi-fair/jikaiLoong/Synthetic_Data_for_ZO/Data/original/2_first_order_full/result/ArcC_MC/llama_3.2_1b/1e-6_synthetic.out
-ERR_0=/grand/sbi-fair/jikaiLoong/Synthetic_Data_for_ZO/Data/original/2_first_order_full/result/ArcC_MC/llama_3.2_1b/1e-6_synthetic.err
+OUT_0=/grand/sbi-fair/jikaiLoong/Synthetic_Data_for_ZO/Data/original/2_first_order_full/result/ArcC_MC/llama_3.2_1b/1e-6_original.out
+ERR_0=/grand/sbi-fair/jikaiLoong/Synthetic_Data_for_ZO/Data/original/2_first_order_full/result/ArcC_MC/llama_3.2_1b/1e-6_original.err
 CUDA_VISIBLE_DEVICES=0,1 MODEL=meta-llama/Llama-3.2-1B MODE=ft TASK=/grand/sbi-fair/jikaiLoong/Synthetic_Data_for_ZO/Data/original/ArcC_MC LR=1e-6 BS=16 STEPS=20000 SEED=0 bash fo_full_finetune_original.sh 1>>$OUT_0 2>>$ERR_0 &
 
-OUT_1=/grand/sbi-fair/jikaiLoong/Synthetic_Data_for_ZO/Data/original/2_first_order_full/result/ArcC_MC/llama_3.2_1b/5e-7_synthetic.out
-ERR_1=/grand/sbi-fair/jikaiLoong/Synthetic_Data_for_ZO/Data/original/2_first_order_full/result/ArcC_MC/llama_3.2_1b/5e-7_synthetic.err
+OUT_1=/grand/sbi-fair/jikaiLoong/Synthetic_Data_for_ZO/Data/original/2_first_order_full/result/ArcC_MC/llama_3.2_1b/5e-7_original.out
+ERR_1=/grand/sbi-fair/jikaiLoong/Synthetic_Data_for_ZO/Data/original/2_first_order_full/result/ArcC_MC/llama_3.2_1b/5e-7_original.err
 CUDA_VISIBLE_DEVICES=2,3 MODEL=meta-llama/Llama-3.2-1B MODE=ft TASK=/grand/sbi-fair/jikaiLoong/Synthetic_Data_for_ZO/Data/original/ArcC_MC LR=5e-7 BS=16 STEPS=20000 SEED=0 bash fo_full_finetune_original.sh 1>>$OUT_1 2>>$ERR_1 &
 
 wait
 
-OUT_2=/grand/sbi-fair/jikaiLoong/Synthetic_Data_for_ZO/Data/original/2_first_order_full/result/ArcC_MC/llama_3.2_1b/2e-7_synthetic.out
-ERR_2=/grand/sbi-fair/jikaiLoong/Synthetic_Data_for_ZO/Data/original/2_first_order_full/result/ArcC_MC/llama_3.2_1b/2e-7_synthetic.err
+OUT_2=/grand/sbi-fair/jikaiLoong/Synthetic_Data_for_ZO/Data/original/2_first_order_full/result/ArcC_MC/llama_3.2_1b/2e-7_original.out
+ERR_2=/grand/sbi-fair/jikaiLoong/Synthetic_Data_for_ZO/Data/original/2_first_order_full/result/ArcC_MC/llama_3.2_1b/2e-7_original.err
 CUDA_VISIBLE_DEVICES=0,1 MODEL=meta-llama/Llama-3.2-1B MODE=ft TASK=/grand/sbi-fair/jikaiLoong/Synthetic_Data_for_ZO/Data/original/ArcC_MC LR=2e-7 BS=16 STEPS=20000 SEED=0 bash fo_full_finetune_original.sh 1>>$OUT_2 2>>$ERR_2 &
 
-OUT_3=/grand/sbi-fair/jikaiLoong/Synthetic_Data_for_ZO/Data/original/2_first_order_full/result/ArcC_MC/llama_3.2_1b/1e-7_synthetic.out
-ERR_3=/grand/sbi-fair/jikaiLoong/Synthetic_Data_for_ZO/Data/original/2_first_order_full/result/ArcC_MC/llama_3.2_1b/1e-7_synthetic.err
+OUT_3=/grand/sbi-fair/jikaiLoong/Synthetic_Data_for_ZO/Data/original/2_first_order_full/result/ArcC_MC/llama_3.2_1b/1e-7_original.out
+ERR_3=/grand/sbi-fair/jikaiLoong/Synthetic_Data_for_ZO/Data/original/2_first_order_full/result/ArcC_MC/llama_3.2_1b/1e-7_original.err
 CUDA_VISIBLE_DEVICES=2,3 MODEL=meta-llama/Llama-3.2-1B MODE=ft TASK=/grand/sbi-fair/jikaiLoong/Synthetic_Data_for_ZO/Data/original/ArcC_MC LR=1e-7 BS=16 STEPS=20000 SEED=0 bash fo_full_finetune_original.sh 1>>$OUT_3 2>>$ERR_3 &
 
 wait
@@ -28,22 +28,22 @@ wait
 
 # cd /home/ubuntu/LLM-inference/jikai-project/Synthetic_Data_for_ZO/PromptZO/MeZO/large_models
 
-# OUT_0=/home/ubuntu/LLM-inference/jikai-project/Synthetic_Data_for_ZO/Data/original/2_first_order_full/result/ArcC_MC/llama_3.2_1b/1e-6_synthetic.out
-# ERR_0=/home/ubuntu/LLM-inference/jikai-project/Synthetic_Data_for_ZO/Data/original/2_first_order_full/result/ArcC_MC/llama_3.2_1b/1e-6_synthetic.err
+# OUT_0=/home/ubuntu/LLM-inference/jikai-project/Synthetic_Data_for_ZO/Data/original/2_first_order_full/result/ArcC_MC/llama_3.2_1b/1e-6_original.out
+# ERR_0=/home/ubuntu/LLM-inference/jikai-project/Synthetic_Data_for_ZO/Data/original/2_first_order_full/result/ArcC_MC/llama_3.2_1b/1e-6_original.err
 # MODEL=meta-llama/Llama-3.2-1B MODE=ft TASK=/home/ubuntu/LLM-inference/jikai-project/Synthetic_Data_for_ZO/Data/original/ArcC_MC LR=1e-6 BS=16 STEPS=20000 SEED=0 bash fo_full_finetune_original.sh 1>>$OUT_0 2>>$ERR_0 &
 
-# OUT_1=/home/ubuntu/LLM-inference/jikai-project/Synthetic_Data_for_ZO/Data/original/2_first_order_full/result/ArcC_MC/llama_3.2_1b/5e-7_synthetic.out
-# ERR_1=/home/ubuntu/LLM-inference/jikai-project/Synthetic_Data_for_ZO/Data/original/2_first_order_full/result/ArcC_MC/llama_3.2_1b/5e-7_synthetic.err
+# OUT_1=/home/ubuntu/LLM-inference/jikai-project/Synthetic_Data_for_ZO/Data/original/2_first_order_full/result/ArcC_MC/llama_3.2_1b/5e-7_original.out
+# ERR_1=/home/ubuntu/LLM-inference/jikai-project/Synthetic_Data_for_ZO/Data/original/2_first_order_full/result/ArcC_MC/llama_3.2_1b/5e-7_original.err
 # MODEL=meta-llama/Llama-3.2-1B MODE=ft TASK=/home/ubuntu/LLM-inference/jikai-project/Synthetic_Data_for_ZO/Data/original/ArcC_MC LR=5e-7 BS=16 STEPS=20000 SEED=0 bash fo_full_finetune_original.sh 1>>$OUT_1 2>>$ERR_1 &
 
 # wait
 
-# OUT_2=/home/ubuntu/LLM-inference/jikai-project/Synthetic_Data_for_ZO/Data/original/2_first_order_full/result/ArcC_MC/llama_3.2_1b/2e-7_synthetic.out
-# ERR_2=/home/ubuntu/LLM-inference/jikai-project/Synthetic_Data_for_ZO/Data/original/2_first_order_full/result/ArcC_MC/llama_3.2_1b/2e-7_synthetic.err
+# OUT_2=/home/ubuntu/LLM-inference/jikai-project/Synthetic_Data_for_ZO/Data/original/2_first_order_full/result/ArcC_MC/llama_3.2_1b/2e-7_original.out
+# ERR_2=/home/ubuntu/LLM-inference/jikai-project/Synthetic_Data_for_ZO/Data/original/2_first_order_full/result/ArcC_MC/llama_3.2_1b/2e-7_original.err
 # MODEL=meta-llama/Llama-3.2-1B MODE=ft TASK=/home/ubuntu/LLM-inference/jikai-project/Synthetic_Data_for_ZO/Data/original/ArcC_MC LR=2e-7 BS=16 STEPS=20000 SEED=0 bash fo_full_finetune_original.sh 1>>$OUT_2 2>>$ERR_2 &
 
-# OUT_3=/home/ubuntu/LLM-inference/jikai-project/Synthetic_Data_for_ZO/Data/original/2_first_order_full/result/ArcC_MC/llama_3.2_1b/1e-7_synthetic.out
-# ERR_3=/home/ubuntu/LLM-inference/jikai-project/Synthetic_Data_for_ZO/Data/original/2_first_order_full/result/ArcC_MC/llama_3.2_1b/1e-7_synthetic.err
+# OUT_3=/home/ubuntu/LLM-inference/jikai-project/Synthetic_Data_for_ZO/Data/original/2_first_order_full/result/ArcC_MC/llama_3.2_1b/1e-7_original.out
+# ERR_3=/home/ubuntu/LLM-inference/jikai-project/Synthetic_Data_for_ZO/Data/original/2_first_order_full/result/ArcC_MC/llama_3.2_1b/1e-7_original.err
 # MODEL=meta-llama/Llama-3.2-1B MODE=ft TASK=/home/ubuntu/LLM-inference/jikai-project/Synthetic_Data_for_ZO/Data/original/ArcC_MC LR=1e-7 BS=16 STEPS=20000 SEED=0 bash fo_full_finetune_original.sh 1>>$OUT_3 2>>$ERR_3 &
 
 # wait
@@ -53,21 +53,21 @@ wait
 # cd /home/jlong1/Downloads/Synthetic_Data_for_ZO/PromptZO/MeZO/large_models
 
 # # independent
-# OUT_0=/home/jlong1/Downloads/Synthetic_Data_for_ZO/Data/original/2_first_order_full/result/ArcC_MC/llama_3.2_1b/1e-6_synthetic.out
-# ERR_0=/home/jlong1/Downloads/Synthetic_Data_for_ZO/Data/original/2_first_order_full/result/ArcC_MC/llama_3.2_1b/1e-6_synthetic.err
+# OUT_0=/home/jlong1/Downloads/Synthetic_Data_for_ZO/Data/original/2_first_order_full/result/ArcC_MC/llama_3.2_1b/1e-6_original.out
+# ERR_0=/home/jlong1/Downloads/Synthetic_Data_for_ZO/Data/original/2_first_order_full/result/ArcC_MC/llama_3.2_1b/1e-6_original.err
 # MODEL=meta-llama/Llama-3.2-1B MODE=ft TASK=/home/jlong1/Downloads/Synthetic_Data_for_ZO/Data/original/ArcC_MC LR=1e-6 BS=16 STEPS=20000 SEED=0 bash fo_full_finetune_original.sh 1>>$OUT_0 2>>$ERR_0 &
 
 # # independent
-# OUT_1=/home/jlong1/Downloads/Synthetic_Data_for_ZO/Data/original/2_first_order_full/result/ArcC_MC/llama_3.2_1b/5e-7_synthetic.out
-# ERR_1=/home/jlong1/Downloads/Synthetic_Data_for_ZO/Data/original/2_first_order_full/result/ArcC_MC/llama_3.2_1b/5e-7_synthetic.err
+# OUT_1=/home/jlong1/Downloads/Synthetic_Data_for_ZO/Data/original/2_first_order_full/result/ArcC_MC/llama_3.2_1b/5e-7_original.out
+# ERR_1=/home/jlong1/Downloads/Synthetic_Data_for_ZO/Data/original/2_first_order_full/result/ArcC_MC/llama_3.2_1b/5e-7_original.err
 # MODEL=meta-llama/Llama-3.2-1B MODE=ft TASK=/home/jlong1/Downloads/Synthetic_Data_for_ZO/Data/original/ArcC_MC LR=5e-7 BS=16 STEPS=20000 SEED=0 bash fo_full_finetune_original.sh 1>>$OUT_1 2>>$ERR_1
 
-# OUT_2=/home/jlong1/Downloads/Synthetic_Data_for_ZO/Data/original/2_first_order_full/result/ArcC_MC/llama_3.2_1b/2e-7_synthetic.out
-# ERR_2=/home/jlong1/Downloads/Synthetic_Data_for_ZO/Data/original/2_first_order_full/result/ArcC_MC/llama_3.2_1b/2e-7_synthetic.err
+# OUT_2=/home/jlong1/Downloads/Synthetic_Data_for_ZO/Data/original/2_first_order_full/result/ArcC_MC/llama_3.2_1b/2e-7_original.out
+# ERR_2=/home/jlong1/Downloads/Synthetic_Data_for_ZO/Data/original/2_first_order_full/result/ArcC_MC/llama_3.2_1b/2e-7_original.err
 # MODEL=meta-llama/Llama-3.2-1B MODE=ft TASK=/home/jlong1/Downloads/Synthetic_Data_for_ZO/Data/original/ArcC_MC LR=2e-7 BS=16 STEPS=20000 SEED=0 bash fo_full_finetune_original.sh 1>>$OUT_2 2>>$ERR_2 &
 
-# OUT_3=/home/jlong1/Downloads/Synthetic_Data_for_ZO/Data/original/2_first_order_full/result/ArcC_MC/llama_3.2_1b/1e-7_synthetic.out
-# ERR_3=/home/jlong1/Downloads/Synthetic_Data_for_ZO/Data/original/2_first_order_full/result/ArcC_MC/llama_3.2_1b/1e-7_synthetic.err
+# OUT_3=/home/jlong1/Downloads/Synthetic_Data_for_ZO/Data/original/2_first_order_full/result/ArcC_MC/llama_3.2_1b/1e-7_original.out
+# ERR_3=/home/jlong1/Downloads/Synthetic_Data_for_ZO/Data/original/2_first_order_full/result/ArcC_MC/llama_3.2_1b/1e-7_original.err
 # MODEL=meta-llama/Llama-3.2-1B MODE=ft TASK=/home/jlong1/Downloads/Synthetic_Data_for_ZO/Data/original/ArcC_MC LR=1e-7 BS=16 STEPS=20000 SEED=0 bash fo_full_finetune_original.sh 1>>$OUT_3 2>>$ERR_3 &
 
 # wait
